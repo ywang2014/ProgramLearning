93ef3970ad31b27e9d6162220c9ca00921ad155e
ywang2014 <ywangsh2014@sina.com>
2015-09-11 23:19:45 +0800

    commit

---
+++ CLInterface/windows.md
@@ -0,0 +1,15 @@
+# windows cmd
+
+tasklist 查看正在运行的进程
+
+taskkill /f /im pid 结束进程
+
+	/f 强制删除		/im 删除镜像	pid进程号
+	
+运行 msconfig  打开系统配置实用程序，查看启动项，可以查看程序所在地址，注册表位置
+
+del /f filename		强制删除文件
+
+dir /ah		查看属性为隐藏文件
+
+del /ah /f filename		删除隐藏属性文件
---
+++ python/RICE/operations.py
@@ -0,0 +1,22 @@
+# -*- coding: uft-8 -*-
+
+# More operation
+def main():
+	hour = 3
+	ones = hour % 10
+	tens = hour // 10
+	print tens, ones
+	print str(tens), str(ones), ":00"
+	print str(tens) + str(ones) + ":00" 
+
+	input("pause!")
+
+# convert a string into numbers using int() float()
+
+	import simplegui
+
+	import math
+
+	import random
+
+	print(math.pi)
