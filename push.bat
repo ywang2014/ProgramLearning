5d92d4a416707473a3edfdc5c92916f3eec8f68c
ywang2014 <ywangsh2014@sina.com>
2016-11-08 13:57:28 +0800

    java

--- push.bat
+++ push.bat
@@ -2,7 +2,7 @@
 "D:\Soft\Git\Git\bin\sh.exe" --login -i
 git add -A
 git status
-git commit -m 'commit'
+git commit -m 'auto-commit'
 git push
 :: 注释方式 :: 		% % 	:
 :%@cmd/k cd/d "E:\Users\ZKY\project\ProgramLearning"%
