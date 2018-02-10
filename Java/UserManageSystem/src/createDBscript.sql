267b764c343e1fdaae467bf143f4141b8ff91504
ywang2014 <ywangsh2014@sina.com>
2018-02-10 22:16:58 +0800

    move Java Project to JavaProject

--- Java/UserManageSystem/src/createDBscript.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE user;	# 删除可能存在的表，再创建新表，安全！
-CREATE TABLE user(
-			id			INT				AUTO_INCREMENT PRIMARY KEY,
-			name		VARCHAR(50)		NOT NULL,
-			sex			VARCHAR(10)		NOT NULL,
-			birthday	DATE
-);
